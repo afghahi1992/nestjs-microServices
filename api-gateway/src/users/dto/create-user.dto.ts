dd255c13f33487acfbbfbfa4010cd8b53b066040
afghahi1992 <afghahi1992@gmail.com>
2023-12-08 10:49:50 +0330

    generate api-gateway

--- api-gateway/src/users/dto/create-user.dto.ts
+++ api-gateway/src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import {IsEmail, IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2, { message: 'Name must have at least 2 characters.' })
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsInt()
+  age: number;
+}
