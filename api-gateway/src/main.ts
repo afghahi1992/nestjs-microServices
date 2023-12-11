3fa17ed1979a681bc69a97c667c2471a274a27de
reza.afghahi <@Password123>
2023-12-11 10:52:10 +0330

    add swagger

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -15,6 +16,15 @@ async function bootstrap() {
     }),
   );
   app.useLogger(app.get(Logger));
+  const config = new DocumentBuilder()
+    .setTitle('api-gateway example')
+    .setDescription('you can test api-gate with swagger')
+    .setVersion('1.0')
+    .addTag('api-gateway')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 
